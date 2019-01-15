ce82e390d0d19023cf9ebd93263ea8f844b116d5
g5 <g5@192.168.25.39>
2019-01-15 15:35:46 +0900

    유저 DAO구버전 잘못올라간것 버전업

--- ExTeamProject/src/dao/UserManagementDAO.java
+++ ExTeamProject/src/dao/UserManagementDAO.java
@@ -5,12 +5,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 
 import db.ConnectionPool;
+import dto.UserManagementDTO;
 import together.ActionForward;
-import userManagement.UserManagementDTO;
+import controller.UserManagementController;
 
 public class UserManagementDAO {
 
@@ -48,33 +51,107 @@ public class UserManagementDAO {
 		} finally {
 			cp.close(con, pstmt, rs);
 		}
+		System.out.println("userLogin()의 리턴값은 " + result + "입니다.");
 		return result;
 	}
-
-	public UserManagementDTO getUserInfo(String user_email){
-		UserManagementDTO umdto = new UserManagementDTO();
-		try {
-			con = cp.getConnection();
-			String sql = "select user_no, user_pass, user_nickname, user_birth, user_gender, user_point, user_phone, user_level from users where user_email=?";
-			pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, user_email);
-			rs = pstmt.executeQuery();
-			if(rs.next()){
-				umdto.setUser_no(rs.getInt("user_no"));
-				umdto.setUser_pass(rs.getString("user_pass"));
-				umdto.setUser_nickname(rs.getString("user_nickname"));
-				umdto.setUser_birth(rs.getInt("user_birth"));
-				umdto.setUser_gender(rs.getString("user_gender"));
-				umdto.setUser_point(rs.getInt("user_point"));
-				umdto.setUser_phone(rs.getString("user_phone"));
-				umdto.setUser_level(rs.getInt("user_level"));
-			}
-		} catch (Exception e) {
-			System.out.print("getUserInfo()메서드 내부 오류: ");
-			e.printStackTrace();
-		} finally {
-			cp.close(con, pstmt, rs);
-		}
-		return umdto;
+	//DB작업 담당
+		private Connection getConnection() throws Exception{
+		Connection con = null;
+		Context init = new InitialContext();
+		DataSource datasource = (DataSource)init.lookup("java:comp/env/jdbc/promotion");
+		con = datasource.getConnection();
+		return con;
 	}
+		//회원가입 메소드 
+		public boolean insertMember(UserManagementDTO mb){
+			
+			con = null;
+			String sql = "";
+			pstmt = null;
+			
+			//회원가입 성공 여부 저장
+			int result = 0;
+			
+			try {
+				con = getConnection(); //DB접속 객체 얻기 
+				
+				/*
+					user_no int(11) AI PK 
+					user_email varchar(30) 
+					user_pass varchar(20) 
+					user_nickname varchar(10) 
+					user_birth int(6) 
+					user_gender varchar(1) 
+					user_point int(11) 
+					user_phone varchar(20) 
+					user_level int(1)
+				 */
+				
+				sql = "insert into users(user_email,user_pass,user_nickname,user_birth,user_gender,user_point,user_phone,user_level)"
+					+ "values(?,?,?,?,?,?,?,?)";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, mb.getUser_email());
+				pstmt.setString(2, mb.getUser_pass());
+				pstmt.setString(3, mb.getUser_nickname());
+				pstmt.setInt(4,mb.getUser_birth());
+				pstmt.setString(5, mb.getUser_gender());
+				pstmt.setInt(6, 0);
+				pstmt.setString(7, "");
+				pstmt.setInt(8, 1);
+				
+				
+				
+				result = pstmt.executeUpdate(); //회원가입 성공 하면 1을 리턴 실패시 0을 리턴
+				
+				//만약에 회원가입에 성공 하면 true리턴
+				if(result != 0){
+					return true;
+				}		
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				
+				try {
+					//자원해제
+					if(pstmt != null){
+						pstmt.close();
+					}
+					if(con != null){
+						con.close();
+					}
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			
+			}
+			return false;
+		}
+		// 아이디 중복체크
+					public int JoinCheck(String user_email){ 
+						Connection con = null;
+						PreparedStatement pre = null;
+						ResultSet rs = null;
+						int check = 0;
+						
+						try {
+							con = getConnection();
+							String sql = "select * from users where user_email=?";
+							pre = con.prepareStatement(sql);
+							pre.setString(1, user_email);
+							rs = pre.executeQuery();
+							if(rs.next()){
+								check = 0;
+							} else { 
+								check = 1; 
+							}
+						} catch (Exception e) { e.printStackTrace(); }
+						finally {
+							if (rs != null) try {rs.close(); } catch(SQLException ex) {ex.printStackTrace(); }
+							if (pre != null) try { pre.close(); } catch(SQLException ex) { ex.printStackTrace(); }
+							if (con != null) try { con.close(); } catch(SQLException ex) { ex.printStackTrace(); }
+						}
+						return check;
+						
+					} 
 }
